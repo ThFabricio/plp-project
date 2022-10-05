88acede617b93545154f12cd916c550bb23a789b
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-05 13:57:07 -0300

    Destroy Scaffold Planner e Usuario

--- app/models/usuario.rb
+++
@@ -1,5 +0,0 @@
-class Usuario < ApplicationRecord
-  validates :userName, presence: true, length: {minimum: 5}
-  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}, uniqueness: true
-  validates :senha, presence: true, length: {minimum: 6}
-end
--- app/views/planners/_planner.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! planner, :id, :created_at, :updated_at
-json.url planner_url(planner, format: :json)
